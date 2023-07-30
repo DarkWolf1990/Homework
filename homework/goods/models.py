8e92120cf5a58c8daf885d251c25c494228cce7a
darkwolf <darkwolf1990janary@gmail.com>
2023-07-30 09:55:41 +0300

    update

--- homework/goods/models.py
+++ homework/goods/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Goods(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Цена')
+    count = models.IntegerField(verbose_name='Количество')
+    add_date = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Дата добавления товара')
--- homework/orders/models.py
+++ homework/orders/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from clients.models import Client
+from goods.models import Goods
 
-# Create your models here.
+
+class Order(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    goods = models.ManyToManyField(Goods)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=8, decimal_places=2)
